86828ae75acc485c669466adbc248a0edb77f918
Connor Allen <connor27allen@gmail.com>
2024-02-26 14:46:56 -0500

    commit_2

--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+if (process.env.NODE_ENV === 'production') 
+    app.use(express.static('../client/dist'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
